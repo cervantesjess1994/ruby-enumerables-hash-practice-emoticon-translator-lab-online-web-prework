b0d26d8e3e2490cf72b0d72a7dac6c63b2f1e8b0
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-08-16 09:44:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,13 @@
 require "yaml"
+YAML.load_file('emoticons.yml')
+
 def load_library
-  puts YAML.load_file('emoticons.yml')
+  hash{}
+  hash.each do|meaning_emoticon,keys|
+    meaning_emoticon{1}={japanese_emoticon}
+    meaning_emoticon{2}={english_emoticon}
+        
+    
 end
 
 def get_japanese_emoticon
